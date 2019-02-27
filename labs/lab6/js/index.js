9aff6b0779998cb4f3cf01ffdd858e7019345110
Carlos J. Garcia Carmona <carlosj.gc989@gmail.com>
2019-02-26 23:20:45 -0700

    Lab 6 submission

--- labs/lab6/js/index.js
+++ labs/lab6/js/index.js
@@ -1,17 +1,31 @@
 function addToList() {
-    let urlList = [];
-    let url = document.getElementById('input-url').value;
     
-    if (url)
-        urlList.push(url);
+    let urlList = [];
+    let url = document.getElementById('url');
+
+    if (url.value != null)
+        urlList.push(url.value);
 
     displayList(urlList);
+    url.value = '';
 }
 
 function displayList(list) {
     const listElement = document.getElementById('list');
 
     list.forEach(function (url) {
-        
+        let prefix = 'http://www.';
+        let link = document.createElement('a');
+        link.href = prefix + url;
+        link.target = "_blank";
+
+        let linkText = url;
+        link.innerHTML = linkText;
+
+        let listItem = document.createElement('li');
+        listItem.className = 'list-inline-item';
+        listItem.append(link);
+
+        listElement.append(listItem);
     });
 }
