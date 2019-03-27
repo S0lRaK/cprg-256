86f1d33561af4e736b2fab036696ebf5860f0bcd
Carlos J. Garcia Carmona <carlosj.gc989@gmail.com>
2019-03-27 17:31:11 -0600

    Display selected items

--- assignments/assignment2/js/main.js
+++ assignments/assignment2/js/main.js
@@ -3,11 +3,12 @@
 window.onload = init;
 
 /* Global variables */
-let customers = {};
+let customer = { customerInfo: {}, orders: [] };
 
 function init() {
 	setCurrentDate();
 	setClickItem();
+	setClickAdd();
 }
 
 function setCurrentDate() {
@@ -40,6 +41,15 @@ function setClickItem() {
 		item.addEventListener("click", event => {
 			setSelectedItem(event, item, itemElements);
 			enableQuantityInput(event);
+		});
+	});
+}
+
+function setClickAdd() {
+	const addButtonElements = document.querySelectorAll(".add-to-order");
+	addButtonElements.forEach(button => {
+		button.addEventListener("click", event => {
+			addToOrder(event, button);
 		});
 	});
 }
@@ -93,7 +103,104 @@ function setSelectedItem(event, selectedItem, items) {
 function enableQuantityInput(event) {
 	const currentOption = event.currentTarget.dataset.option;
 	const currentOptionElement = document.querySelector("#" + currentOption);
-	const quantityInputElement = currentOptionElement.querySelector("input[name='quantity']");
+	const quantityInputElement = currentOptionElement.querySelector(
+		"input[name='quantity']"
+	);
 
 	quantityInputElement.removeAttribute("disabled");
+}
+
+function addToOrder(event, button) {
+	if (event) {
+		const currentOption = button.dataset.option;
+		const currentOptionElement = document.querySelector("#" + currentOption);
+
+		const selectedItem = currentOptionElement.querySelector(".item.selected");
+		const selectedItemTitle = selectedItem.querySelector(".title").innerText;
+
+		let individualPrice;
+
+		if (currentOptionElement.id == "sandwich") {
+			individualPrice = selectedItem.querySelector(".price").dataset.price;
+		} else {
+			individualPrice = currentOptionElement.querySelector(".size select")
+				.value;
+		}
+
+		let numExtras = 0;
+		let toppingsList = [];
+
+		if (currentOptionElement.id == "pizza") {
+			const toppings = currentOptionElement.querySelectorAll(
+				".topping input:checked"
+			);
+			numExtras = toppings.length;
+			toppings.forEach(topping => {
+				toppingsList.push(topping.value);
+			});
+		}
+
+		const quantity = currentOptionElement.querySelector(".quantity input")
+			.value;
+
+		const price = individualPrice * quantity + numExtras * prices.topping;
+
+		let order = {
+			option: currentOption,
+			item: selectedItemTitle,
+			quantity: quantity,
+			price: price,
+			extras: toppingsList
+		};
+
+		customer.orders.push(order);
+
+		displayOrders();
+		showContactSection();
+		showOrderButton();
+	}
+}
+
+function showContactSection() {
+	const contactForm = document.getElementById("contact");
+	contactForm.classList.remove("hidden");
+}
+
+function displayOrders() {
+	const list = document.querySelector("#order ol");
+
+	list.innerHTML = "";
+
+	customer.orders.forEach(order => {
+		const listItem = document.createElement("li");
+		listItem.setAttribute("class", "list-group-item d-flex flex-row justify-content-between");
+
+		const quantityElement = document.createElement("span");
+		quantityElement.innerText = order.quantity + " ";
+
+		const itemElement = document.createElement("span");
+		const itemElementTitle = document.createElement("span");
+		itemElementTitle.innerText = order.item;
+		itemElement.appendChild(itemElementTitle);
+
+		let extrasElement = "";
+
+		if (order.option == "pizza" && order.extras.length > 0) {
+			extrasElement = document.createElement("span");
+			extrasElement.innerText = ", " + order.extras;
+			itemElement.appendChild(extrasElement);
+		}
+
+		const priceElement = document.createElement("span");
+		priceElement.innerText = " $" + order.price;
+
+		listItem.append(quantityElement, itemElement, priceElement);
+
+		list.append(listItem);
+	})
+}
+
+function showOrderButton() {
+	const orderButton = document.getElementById("checkout");
+	orderButton.classList.remove("hidden");
 }
