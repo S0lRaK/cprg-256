e5c055122d45652b0cd0af65f73ca7f9eb8b13da
Carlos J. Garcia Carmona <carlosj.gc989@gmail.com>
2019-03-27 12:40:21 -0600

    Item selection

--- assignments/assignment2/js/main.js
+++ assignments/assignment2/js/main.js
@@ -1,6 +1,16 @@
 "use strict";
 
-window.onload = function() {
+window.onload = init;
+
+/* Global variables */
+let customers = {};
+
+function init() {
+	setCurrentDate();
+	setClickItem();
+}
+
+function setCurrentDate() {
 	let month = new Array(
 		"January",
 		"February",
@@ -15,19 +25,24 @@ window.onload = function() {
 		"November",
 		"December"
 	);
-
 	let today = new Date();
 	let dd = today.getDate();
 	let mm = today.getMonth();
 	let currentMonth = month[mm];
 	let yyyy = today.getFullYear();
 	today = currentMonth + " " + dd + ", " + yyyy;
-
 	document.getElementById("current-date").innerHTML = today;
-};
+}
 
-/* Global variables */
-let customers = {};
+function setClickItem() {
+	const itemElements = document.querySelectorAll(".item");
+	itemElements.forEach(item => {
+		item.addEventListener("click", event => {
+			setSelectedItem(event, item, itemElements);
+			enableQuantityInput(event);
+		});
+	});
+}
 
 function showSection(section) {
 	hideOtherSections(section);
@@ -48,14 +63,37 @@ function hideOtherSections(visibleSection) {
 
 function enableAddToOrder(item) {
 	const itemDivElement = document.getElementById(item);
-	const quantityInputElement = itemDivElement.querySelector("input[name='quantity']");
+	const quantityInputElement = itemDivElement.querySelector(
+		"input[name='quantity']"
+	);
+	const itemSelected = itemDivElement.querySelector(".selected");
 	const buttonElement = itemDivElement.querySelector("button");
 
-	if (quantityInputElement.value > 0) {
+	if (quantityInputElement.value > 0 && itemSelected) {
 		buttonElement.classList.remove("disabled");
 		buttonElement.removeAttribute("disabled");
 	} else {
 		buttonElement.classList.add("disabled");
 		buttonElement.setAttribute("disabled", "");
 	}
+}
+
+function setSelectedItem(event, selectedItem, items) {
+	if (event) {
+		items.forEach(item => {
+			if (selectedItem == item) {
+				item.classList.add("selected");
+			} else if (item.dataset.option == selectedItem.dataset.option) {
+				item.classList.remove("selected");
+			}
+		});
+	}
+}
+
+function enableQuantityInput(event) {
+	const currentOption = event.currentTarget.dataset.option;
+	const currentOptionElement = document.querySelector("#" + currentOption);
+	const quantityInputElement = currentOptionElement.querySelector("input[name='quantity']");
+
+	quantityInputElement.removeAttribute("disabled");
 }
